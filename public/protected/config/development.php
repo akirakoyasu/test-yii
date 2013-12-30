5dbbf33c7193bf62056154b9ed8cac4b77450c16
Akira Koyasu <mail@akirakoyasu.net>
2013-12-30 09:21:07 +0900

    move gii config to development

--- public/protected/config/development.php
+++ public/protected/config/development.php
@@ -2,5 +2,15 @@
 
 return CMap::mergeArray(
     require(__DIR__ . '/main.php'),
-    array()
+    array(
+        'modules' => array(
+            'gii' => array(
+                'class' => 'system.gii.GiiModule',
+                'password' => 'password',
+                'ipFilters' => array('*'),
+//            'newFileMode'=>0666,
+//            'newDirMode'=>0777,
+            ),
+        ),
+    )
 );
